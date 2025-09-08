ebccc7c11da329bcd1e56be34a31ca0087201770
Yoshimitsu <jayiscotton@gmail.com>
2025-09-08 16:55:15 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,129 @@
+function New() {
+    var img = document.getElementById("myImage");
+    img.src = "Holiday-Images/NewYear.png";
+    document.getElementById("HolidayName").innerHTML = "<b>NEW YEAR'S DAY</b>";
+    document.getElementById("MonthDay").textContent = "JANUARY 1ST";
+    document.getElementById("Description").innerHTML = "Marks the first day of the Gregorian calendar year.<br> Families gather, share meals, and celebrate with fireworks<br>as a symbol of renewal and new beginnings.";
+}
+
+function Valor() {
+    var img = document.getElementById("myImage");
+    img.src = "Holiday-Images/ArawNgKagitingan.png";
+    document.getElementById("HolidayName").innerHTML = "<b>ARAW NG KAGITINGAN</b>";
+    document.getElementById("MonthDay").textContent = "APRIL 9TH";
+    document.getElementById("Description").innerHTML = "Commemorates the heroism of Filipino and American soldiers <br> during World War II, especially the Bataan Death March. <br> Ceremonies are held at Mt. Samat National Shrine in Bataan.";
+}
+
+function Maundy() {
+    var img = document.getElementById("myImage");
+    img.src = "Holiday-Images/MaundyThursday.png";
+    document.getElementById("HolidayName").innerHTML = "<b>MAUNDY THURSDAY</b>";
+    document.getElementById("MonthDay").textContent = "APRIL 13TH";
+    document.getElementById("Description").innerHTML = "Observed during Holy Week, it recalls the Last Supper of Jesus Christ with his <br> disciples. Many attend Mass and join religious traditions like the Visita Iglesia.";
+}
+
+function Good() {
+    var img = document.getElementById("myImage");
+    img.src = "Holiday-Images/GoodFriday.png";
+    document.getElementById("HolidayName").innerHTML = "<b>GOOD FRIDAY</b>";
+    document.getElementById("MonthDay").textContent = "APRIL 14TH";
+    document.getElementById("Description").innerHTML = "A solemn day commemorating the crucifixion of Jesus Christ. <br> Many Filipinos participate in religious processions and reenactments.";
+}
+
+function Labor() {
+    var img = document.getElementById("myImage");
+    img.src = "Holiday-Images/LaborDay.png";
+    document.getElementById("HolidayName").innerHTML = "<b>LABOR DAY</b>";
+    document.getElementById("MonthDay").textContent = "MAY 1ST";
+    document.getElementById("Description").innerHTML = "A day to honor the contributions of workers and laborers. <br> Parades, rallies, and various events are held <br> nationwide to celebrate the Filipino workforce.";
+}
+
+function Independence() {
+    var img = document.getElementById("myImage");
+    img.src = "Holiday-Images/IndependenceDay.png";
+    document.getElementById("HolidayName").innerHTML = "<b>INDEPENDENCE DAY</b>";
+    document.getElementById("MonthDay").textContent = "JUNE 12TH";
+    document.getElementById("Description").innerHTML = "Commemorates the declaration of Philippine independence <br> from Spanish rule in 1898. Festivities include flag-raising ceremonies, parades, and cultural events.";
+}
+
+function EidUlFitr() {
+    var img = document.getElementById("myImage");
+    img.src = "Holiday-Images/EidUl-Fitr.png";
+    document.getElementById("HolidayName").innerHTML = "<b>EID UL-FITR</b>";
+    document.getElementById("MonthDay").textContent = "OCTOBER 23RD";
+    document.getElementById("Description").innerHTML = "A significant Islamic holiday marking the end of Ramadan, <br> the holy month of fasting. It is celebrated with prayers, feasts, and giving of alms to the needy.";
+}
+
+function Hero() {
+    var img = document.getElementById("myImage");
+    img.src = "Holiday-Images/NationalHeroesDay.png";
+    document.getElementById("HolidayName").innerHTML = "<b>NATIONAL HEROES DAY</b>";
+    document.getElementById("MonthDay").textContent = "AUGUST 28TH";
+    document.getElementById("Description").innerHTML = "Honors the bravery and sacrifices of Filipino heroes <br> who fought for the country's freedom and independence. <br> Ceremonies are held at the Libingan ng mga Bayani (Cemetery of Heroes).";
+}
+
+function Saint() {
+    var img = document.getElementById("myImage");
+    img.src = "Holiday-Images/AllSaintsDay.png";
+    document.getElementById("HolidayName").innerHTML = "<b>ALL SAINTS' DAY</b>";
+    document.getElementById("MonthDay").textContent = "NOVEMBER 1ST";
+    document.getElementById("Description").innerHTML = "A day to honor all saints, known and unknown. <br> Filipinos visit cemeteries to pay respects to deceased loved ones, <br> often bringing flowers and lighting candles.";
+}
+
+function Boni() {
+    var img = document.getElementById("myImage");
+    img.src = "Holiday-Images/BonifacioDay.png";
+    document.getElementById("HolidayName").innerHTML = "<b>BONIFACIO DAY</b>";
+    document.getElementById("MonthDay").textContent = "NOVEMBER 30TH";
+    document.getElementById("Description").innerHTML = "Commemorates the birth of Andrés Bonifacio, <br> a national hero and founder of the Katipunan. <br> Ceremonies and events are held to honor his legacy.";
+}
+
+function Christ() {
+    var img = document.getElementById("myImage");
+    img.src = "Holiday-Images/Christmas.png";
+    document.getElementById("HolidayName").innerHTML = "<b>CHRISTMAS DAY</b>";
+    document.getElementById("MonthDay").textContent = "DECEMBER 25TH";
+    document.getElementById("Description").innerHTML = "Celebrates the birth of Jesus Christ. <br> Filipinos observe the season with festive decorations, <br> family gatherings, gift-giving, and religious services.";
+}
+
+function Rizal() {
+    var img = document.getElementById("myImage");
+    img.src = "Holiday-Images/RizalDay.png";
+    document.getElementById("HolidayName").innerHTML = "<b>RIZAL DAY</b>";
+    document.getElementById("MonthDay").textContent = "DECEMBER 30TH";
+    document.getElementById("Description").innerHTML = "Honors the life and works of Dr. José Rizal, <br> the national hero of the Philippines. <br> Ceremonies are held at Rizal Park in Manila and other locations.";
+}
+
+// Easter Eggs
+function Birthday() {
+    alert("EASTER EGG FOUND!");
+    var img = document.getElementById("myImage");
+    img.src = url = "https://facts.net/wp-content/uploads/2023/05/birthday-cake-1536x1024.jpg";
+    document.getElementById("HolidayName").innerHTML = "<b>JAY'S BIRTHDAY</b>";
+    document.getElementById("MonthDay").textContent = "FEBRUARY 11TH";
+    document.getElementById("Description").textContent = "Celebration of Jay's birthday.";
+}
+
+function WTC() {
+    alert("EASTER EGG FOUND!");
+    var img = document.getElementById("myImage");
+    img.src = url = "https://www.masslive.com/resizer/f0MuItf39PBYNr1rUF6WLWz489s=/1280x0/smart/advancelocal-adapter-image-uploads.s3.amazonaws.com/image.masslive.com/home/mass-media/width2048/img/breakingnews/photo/2013/04/2001-terrorist-attackjpg-38057e3ac0ca68f5.jpg";
+    document.getElementById("HolidayName").innerHTML = "<b>WORLD TRADE CENTER</b>";
+    document.getElementById("MonthDay").textContent = "ATTACK ON SEPTEMBER 11TH";
+    document.getElementById("Description").textContent = "Remembering the tragic events of September 11, 2001.";
+}
+
+function Darkweb() {
+    alert("167.254.13.666 Isn't safe, Enter anyway?");
+    window.open("https://www.lomando.com/main.html");
+    var img = document.getElementById("myImage");
+    img.src = url = "https://i.pinimg.com/originals/64/aa/a2/64aaa23ecc4fd510dba440161cb04999.jpg";
+    document.getElementById("HolidayName").innerHTML = "<b>167.254.13.666</b>";
+    document.getElementById("MonthDay").innerHTML = "<i>Unknown</i>";
+    document.getElementById("Description").innerHTML = "<i>Unknown</i>";
+    document.getElementsByClassName("container")[0].style.backgroundColor = "rgba(0, 0, 0, 0)";
+    document.getElementsByClassName("calendar")[0].style.backgroundColor = "rgba(0, 0, 0, 0)";
+    // For Tags
+    document.body.style.backgroundImage = "url('https://wallpaperaccess.com/full/7069678.jpg')";
+    document.body.style.color = "black";
+}
